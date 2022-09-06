d2de3faeafd108034283d0e16197264a2e0c1d48
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-06 20:47:49 +0100

    dashboard js dne

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -17,3 +17,41 @@ router.get("/", async (req, res) => {
       console.log(err);
     }
   });
+
+  router.get("/posts/:id", async (req, res) => {
+    try {
+      const postData = await BlogPost.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+          },
+          {
+            model: Comment,
+          }
+        ],
+      });
+  
+      const posts = postData.get({ plain: true });
+      console.log(posts, "received")
+      res.render("posts", {
+        posts,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect("/");
+    }
+    res.render("login");
+  });
+  
+  router.get('/createBlog', (req, res) => {
+    res.render('createBlog');
+  });
+  
+  
+  
+  module.exports = router;
