a98dbcb015cd0fb2bdf6c6630eee2b90ea0766b9
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-06 20:47:03 +0100

    started routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
 const { User, BlogPost, Comment } = require("../models");
 const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+    try {
+      const posts = await BlogPost.findAll({
+        include: [User],
+      });
+  
+      const postData = posts.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        postData,
+        user_id: req.session.user_id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
